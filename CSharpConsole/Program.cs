59818a74710782dfb186e58795e103ef89a04220
CoJangKeun <bbh1039@naver.com>
2021-01-05 17:58:49 +0900

    == Tank Coper Car Added ==

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double a = 20.5f;
+            object b = (int)a;
+
+            Console.WriteLine(b);
+
+            int? c = null;
+            Console.WriteLine(c ?? 0);
+
+            if (c.HasValue)
+            {
+                Console.WriteLine(c);
+            }
+            else
+            {
+                Console.WriteLine("NULL");
+            }
+
+            var d = 3.141592;
+            Console.WriteLine("Type: {0}, Value: {1}", d.GetType(), d);
+
+            var f = new int[] { 10, 20, 30 };
+            Console.WriteLine("Type: {0}", f.GetType());
+
+            Mammal mammal = new Mammal();
+            mammal.Nurse();
+
+            mammal = new Dog();
+            mammal.Nurse();
+
+            Dog dog = new Dog();
+            dog.Nurse();
+            dog.Bark();
+
+            mammal = new Cat();
+            mammal.Nurse();
+
+            Cat cat = (Cat)mammal;
+            cat.Nurse();
+            cat.Meow();
+        }
+    }
+
+    class Mammal
+    {
+        public void Nurse() { Console.WriteLine("Nurse"); }
+    }
+
+    class Dog : Mammal
+    {
+        public void Bark() { Console.WriteLine("Bark"); }
+    }
+    class Cat : Mammal
+    {
+        public void Meow() { Console.WriteLine("Meow"); }
+    }
+}
---
+++ First Project/Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    [Range (0.1f, 10)]
+    private float moveSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void Move()
+    {
+        float moveDelta = moveSpeed * Time.deltaTime;
+        Vector3 pos = transform.position;
+        pos.z += moveDelta;
+        transform.position = pos;
+    }
+
+    private void Move1()
+    {
+        float moveDelta = moveSpeed * Time.deltaTime;
+        transform.Translate(Vector3.up * moveDelta);
+    }
+
+    void Move(Vector3 dir)
+    {
+        float moveDelta = moveSpeed * Time.deltaTime;
+        transform.Translate(dir * moveDelta);
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Move(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(Vector3.right);
+        }
+    }
+}
