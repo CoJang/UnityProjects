01da1a054a5823412558732509ce4e946dd1519d
CoJangKeun <bbh1039@naver.com>
2021-01-06 17:59:43 +0900

    == Auto Race Added ==

---
+++ First Project/Assets/Scripts/AutoRaceCar.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRaceCar : Car
+{
+    [Range(0, 50)]
+    public float RayRange = 5.0f;
+    private Ray[] rays = new Ray[3];
+    private RaycastHit[] rayHits = new RaycastHit[3];
+
+    private float originSpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            rays[i] = new Ray(transform.position, transform.forward);
+        }
+
+        originSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RayUpdate();
+        RayProcess();
+    }
+
+    void RayUpdate()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            rays[i].origin = transform.position;
+        }
+        rays[0].direction = transform.forward;
+        Quaternion rot = Quaternion.Euler(0, 45, 0);
+        rays[1].direction = rot * transform.forward;
+        rot = Quaternion.Euler(0, -45, 0);
+        rays[2].direction = rot * transform.forward;
+    }
+
+    void RayProcess()
+    {
+        bool isLeftHit = false;
+        bool isRightHit = false;
+        bool isFrontHit = false;
+
+        if (Physics.Raycast(rays[0], out rayHits[0], RayRange))
+        {
+            moveSpeed = originSpeed * 0.5f;
+            isFrontHit = true;
+        }
+        else
+            moveSpeed = originSpeed;
+
+        if (Physics.Raycast(rays[1], out rayHits[1], RayRange))
+        {
+            //Curve(false);
+            isLeftHit = true;
+        }
+        if (Physics.Raycast(rays[2], out rayHits[2], RayRange))
+        {
+            //Curve(true);
+            isRightHit = true;
+        }
+
+        if (!isLeftHit && !isRightHit)
+        {
+            ResetWheels();
+        }
+        else if (isLeftHit && isRightHit)
+        {
+            if(rayHits[1].distance > rayHits[2].distance && isFrontHit)
+            {
+                Curve(true);
+            }
+            else if (rayHits[1].distance < rayHits[2].distance && isFrontHit)
+            {
+                Curve(false);
+            }
+        }
+        else if(isLeftHit)
+        {
+            Curve(false);
+        }
+        else if(isRightHit)
+        {
+            Curve(true);
+        }
+
+        Move(true);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(rays[0].origin, rays[0].direction * RayRange, Color.red);
+        Debug.DrawRay(rays[1].origin, rays[1].direction * RayRange, Color.yellow);
+        Debug.DrawRay(rays[2].origin, rays[2].direction * RayRange, Color.green);
+    }
+}
---
+++ First Project/Assets/Scripts/Car.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1.0f, 20)]
+    protected float moveSpeed = 3.0f;
+
+    [SerializeField]
+    private GameObject[] Wheels = new GameObject[4];
+    [SerializeField]
+    protected float Sensitivity = 1.0f;
+
+    private float wheelAngle = 0;
+    private float wheelMaxAngle = 50.0f;
+    private Vector3 moveDir = Vector3.forward;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Move(true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Curve(false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Curve(true);
+        }
+    }
+
+    protected void Move(bool isForward)
+    {
+        int drive = -1;
+        if (isForward) drive = 1;
+
+        Quaternion rot = Quaternion.Euler(0, (wheelAngle * drive) * Time.deltaTime, 0);
+        moveDir = rot * moveDir;
+        transform.position += (moveDir * moveSpeed * Time.deltaTime) * drive;
+        transform.rotation *= rot;
+
+        foreach (GameObject wheels in Wheels)
+        {
+            wheels.transform.Rotate(Vector3.up, moveSpeed, Space.Self);
+        }
+    }
+
+    protected void Curve(bool isRight)
+    {
+        int dir = -1;
+        if (isRight) dir = 1;
+        if (Mathf.Abs(wheelAngle) < wheelMaxAngle)
+        {
+            wheelAngle += Sensitivity * dir;
+            Mathf.Clamp(wheelAngle, -wheelMaxAngle, wheelMaxAngle);
+            Wheels[0].transform.Rotate(Vector3.up, Sensitivity * dir, Space.World);
+            Wheels[1].transform.Rotate(Vector3.up, Sensitivity * dir, Space.World);
+        }
+    }
+
+    protected void ResetWheels()
+    {
+        if(wheelAngle > 0)
+        {
+            wheelAngle -= Sensitivity * 3.0f;
+            if (wheelAngle < 0) wheelAngle = 0;
+            Wheels[0].transform.Rotate(Vector3.up, -Sensitivity * 3.0f, Space.World);
+            Wheels[1].transform.Rotate(Vector3.up, -Sensitivity * 3.0f, Space.World);
+        }
+        else if (wheelAngle < 0)
+        {
+            wheelAngle += Sensitivity * 3.0f;
+            if (wheelAngle > 0) wheelAngle = 0;
+            Wheels[0].transform.Rotate(Vector3.up, Sensitivity * 3.0f, Space.World);
+            Wheels[1].transform.Rotate(Vector3.up, Sensitivity * 3.0f, Space.World);
+        }
+    }
+}
