f2cff2c79b05e9869be9b4b62c0b1972448618fb
CoJangKeun <bbh1039@naver.com>
2021-02-16 00:10:53 +0900

    == CrossHair Added ==

--- RenderPipelineProject/Assets/Scripts/Game/PlayerManager.cs
+++ RenderPipelineProject/Assets/Scripts/Game/PlayerManager.cs
@@ -7,9 +7,12 @@ using System.IO;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject topdownCamera;
+    [SerializeField] GameObject crossHairPrefab;
+
     PhotonView PV;
     GameObject controller;
     GameObject playerCamera;
+    GameObject crossHair;
 
     private void Awake()
     {
@@ -32,6 +35,7 @@ public class PlayerManager : MonoBehaviour
                         spawnPoint.position, spawnPoint.rotation, 0, new object[] { PV.ViewID });
 
         playerCamera = Instantiate(topdownCamera, spawnPoint.position, Quaternion.Euler(60, 0, 0));
+        //crossHair = Instantiate(crossHairPrefab, spawnPoint.position, Quaternion.Euler(60, 0, 0));
 
         playerCamera.GetComponent<CameraFollow>().SetTarget(controller.transform);
         controller.GetComponent<PlayerController>().BindPlayerCamera(playerCamera.GetComponentInChildren<Camera>());
